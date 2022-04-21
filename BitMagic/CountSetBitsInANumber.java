64b3d6756861fd750f9ccc4d709ce224dd51e1db
Niraj Ramakant Chaudhari <nirajchaudhari8698@gmail.com>
2022-04-21 22:33:37 +0530

    Count Set Bits In A Number Program committed.

---
+++ BitMagic/CountSetBitsInANumber.java
@@ -0,0 +1,42 @@
+package BitMagic;
+
+public class CountSetBitsInANumber extends BitMagicParentClass {
+
+    public static void main(String[] args) {
+
+        int num = getNumber();
+
+        // Time Complexity O(N) where N is total number of bits in the number
+        // approachOne(num);
+
+        // More efficient O(S) where S is only the number of set bits in the number
+        approachTwo(num);
+
+    }
+
+    static void approachOne(int num) {
+        int count = 0, tempNum = num;
+
+        do {
+            if ((tempNum & 1) == 1) {
+                count++;
+            }
+
+            tempNum = tempNum >> 1;
+        } while (tempNum > 0);
+
+        System.out.println("\nTotal number of set bits in the number " + num + " are : " + count + "\n");
+    }
+
+    static void approachTwo(int num) {
+
+        int count = 0, tempNum = num;
+
+        do {
+            count++;
+            tempNum = tempNum & (tempNum - 1);
+        } while (tempNum > 0);
+
+        System.out.println("\nTotal number of set bits in the number " + num + " are : " + count + "\n");
+    }
+}
