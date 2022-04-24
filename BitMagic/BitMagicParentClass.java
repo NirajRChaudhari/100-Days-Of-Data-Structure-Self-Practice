4405a99481b6e02de66c6cd2d4cd75372ba8632d
Niraj Ramakant Chaudhari <nirajchaudhari8698@gmail.com>
2022-04-24 22:33:37 +0530

    Bit Magic Parent Class Program committed.

---
+++ BitMagic/BitMagicParentClass.java
@@ -0,0 +1,27 @@
+package BitMagic;
+
+import java.util.Scanner;
+
+public class BitMagicParentClass {
+
+    static Scanner sc = new Scanner(System.in);
+
+    static int getNumber() {
+        System.out.println("\nEnter the Number : ");
+        return sc.nextInt();
+    }
+
+    protected static int[] getArray() {
+        System.out.println("Please enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the values of the array: ");
+        for (int i = 0; i < size; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        return array;
+    }
+}
