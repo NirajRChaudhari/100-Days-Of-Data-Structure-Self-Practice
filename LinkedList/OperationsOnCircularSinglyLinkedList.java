134a06f83c17fa693c929a709ee2c143e06268b5
Niraj Ramakant Chaudhari <nirajchaudhari8698@gmail.com>
2022-06-16 22:33:34 +0530

    Operations On Circular Singly Linked List Program committed.

---
+++ LinkedList/OperationsOnCircularSinglyLinkedList.java
@@ -0,0 +1,153 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+import LinkedList.CircularSinglyLinkedList.Node;
+
+public class OperationsOnCircularSinglyLinkedList {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        CircularSinglyLinkedList linkedList = new CircularSinglyLinkedList();
+        String choice;
+
+        do {
+            System.out.println("\n\n************ START MENU *************");
+
+            System.out.println(
+                    "\nSelect the Operation : \n1. Insertion\n2. Deletion\n3. Search");
+
+            int operation = sc.nextInt();
+
+            switch (operation) {
+                case 1:
+                    linkedList.head = insertionOperation(linkedList);
+                    linkedList.print();
+                    break;
+
+                case 2:
+                    // linkedList.head = deletionOperation(linkedList);
+                    linkedList.print();
+                    break;
+
+                case 3:
+                    // searchOperation(linkedList);
+            }
+
+            System.out.println("Do you want to continue (Y/N) ? ");
+            choice = sc.next();
+
+        } while (choice.equals("y") || choice.equals("Y"));
+
+    }
+
+    // We can get O(1) time complexity for insertion at start and end if we store
+    // tailNode instead of head
+    static Node insertionOperation(CircularSinglyLinkedList linkedList) {
+        System.out.println("\nEnter the Element to be inserted : ");
+        int data = sc.nextInt();
+
+        System.out.println(
+                "\nSelect option where you want Insert the element : \n1. At the Start\n2. At the End\n3. In Between");
+
+        int position = sc.nextInt();
+
+        switch (position) {
+            case 1:
+                linkedList.head = insertAtStart(linkedList.head, data);
+                break;
+
+            case 2:
+                linkedList.head = insertAtEnd(linkedList.head, data);
+                break;
+
+            case 3:
+                System.out.println("\nEnter the index where element is to be added (Starts with 0) : ");
+                int index = sc.nextInt();
+
+                linkedList.head = insertInBetween(linkedList.head, data, index);
+                break;
+        }
+
+        return linkedList.head;
+    }
+
+    static Node insertAtStart(Node head, int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            head.next = head;
+
+        } else {
+
+            Node tempNode = head;
+            while (tempNode.next != head) {
+                tempNode = tempNode.next;
+            }
+
+            tempNode.next = newNode;
+            newNode.next = head;
+
+            head = newNode;
+        }
+
+        return head;
+    }
+
+    static Node insertAtEnd(Node head, int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            head.next = head;
+
+        } else {
+            Node tempNode = head;
+            while (tempNode.next != head) {
+                tempNode = tempNode.next;
+            }
+
+            tempNode.next = newNode;
+            newNode.next = head;
+        }
+
+        return head;
+    }
+
+    static Node insertInBetween(Node head, int data, int index) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+
+        } else if (index == 0) {
+            head = insertAtStart(head, data);
+
+        } else {
+            Node tempNode = head;
+
+            int i = 1;
+            tempNode = tempNode.next;
+
+            // If you start with first node tempNode != head will fail
+            while (i < index && tempNode != head) {
+
+                if (i == index - 1) {
+
+                    newNode.next = tempNode.next;
+                    tempNode.next = newNode;
+                }
+
+                i++;
+                tempNode = tempNode.next;
+            }
+        }
+
+        return head;
+    }
+}
