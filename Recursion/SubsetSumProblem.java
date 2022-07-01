12ddf6e32779cc1dcfa919d4bcfe7f6e0497cc7f
Niraj Ramakant Chaudhari <nirajchaudhari8698@gmail.com>
2022-07-01 22:33:33 +0530

    Subset Sum Problem Program committed.

---
+++ Recursion/SubsetSumProblem.java
@@ -0,0 +1,30 @@
+package Recursion;
+
+public class SubsetSumProblem extends RecursionParentClass {
+
+    public static void main(String[] args) {
+
+        int[] array = getArray();
+
+        System.out.println("\nEnter the Sum : ");
+        int inputSum = sc.nextInt();
+
+        System.out.println("\nTotal No. of subsets with sum " + inputSum + " are : "
+                + getTotalNoOfSubsetsWithGivenSum(array, inputSum, 0) + "\n");
+
+    }
+
+    static int getTotalNoOfSubsetsWithGivenSum(int[] array, int inputSum, int index) {
+
+        if (inputSum == 0) {
+            return 1;
+        }
+
+        if (index == array.length || inputSum < 0) {
+            return 0;
+        }
+
+        return getTotalNoOfSubsetsWithGivenSum(array, inputSum, index + 1)
+                + getTotalNoOfSubsetsWithGivenSum(array, inputSum - array[index], index + 1);
+    }
+}
