55eaae7da0e4733b0963e64844e75bcad1f854c4
Niraj Ramakant Chaudhari <nirajchaudhari8698@gmail.com>
2022-07-17 22:33:32 +0530

    String Parent Class Program committed.

---
+++ Strings/StringParentClass.java
@@ -0,0 +1,20 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class StringParentClass {
+
+    static Scanner sc = new Scanner(System.in);
+
+    /*
+     * String are of 3 types:
+     * 1. String (immutable, thread safe as new object is created for every change)
+     * 2. StringBuffer (mutable, thread safe)
+     * 3. StringBuilder (mutable, thread unsafe)
+     * 
+     * 
+     * For String type, literal pool optimizes memory by avoiding reallocating
+     * memory to already existing string and instead new object also points to same
+     * memory literal
+     */
+}
